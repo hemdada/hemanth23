5a999951ef00f44c13c8cb23153fa8c397c9678c
Hemanth Reddy <hemanth.sannareddy@wavemaker.com>
2016-11-22 15:43:58 +0530

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "flyer",
+  "defaultLanguage" : "en",
+  "displayName" : "hemanth23",
+  "homePage" : "Main",
+  "name" : "hemanth23",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
